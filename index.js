27113bbad3fe9a4024b7ac34a4f6a885db2df16d
Sanjaykumar Rathva <sanjayrathva091@gmail.com>
2025-02-28 18:46:38 +0000

    Implemented User model with username storage

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
-console.log(process.env.MONGO_URI)
+
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB Connected'))
