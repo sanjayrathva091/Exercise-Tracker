1aa7f773333c6d6e349f27737bf9a22870bd8fe9
Sanjaykumar Rathva <sanjayrathva091@gmail.com>
2025-02-28 18:42:18 +0000

    Added MongoDB connection using Mongoose

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const express = require('express')
+const mongoose = require('mongoose');
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
 
 app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
-
+console.log(process.env.MONGO_URI)
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.error(err));
 
 
 
